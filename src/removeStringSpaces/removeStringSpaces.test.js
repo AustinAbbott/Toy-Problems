4caf6873178efb0bef43277019d525a3c717a899
Austin Abbott <magicaustin51547@gmail.com>
2020-05-18 11:13:40 -0600

    Add additional test to removeStringSpaces

--- src/removeStringSpaces/removeStringSpaces.test.js
+++ src/removeStringSpaces/removeStringSpaces.test.js
@@ -2,6 +2,8 @@ const removeStringSpaces = require('./removeStringSpaces');
 
 test('removes spaces from a given string', () => {
   expect(removeStringSpaces('a us  t in')).toBe('austin');
-  expect(removeStringSpaces('b ob')).toBe('bob');
-  expect(removeStringSpaces('mary')).toBe('mary');
 });
+
+test('if no spaces, leaves the string as is', () => {
+    expect(removeStringSpaces('hello')).toBe('hello');
+  });
